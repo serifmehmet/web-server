a8c3eb2b9bbb47545fcebbcc72f735a74959f0ad
Mehmet Tozlu <mehmetserif@gmail.com>
2016-01-15 23:12:52 +0200

    Added Middleware Module

---
+++ middleware.js
@@ -0,0 +1,14 @@
+var middleware = {
+    requireAuthentication: function(req, res, next) {
+        console.log('private route hit!');
+        next();
+    },
+    
+    logger: function(req, res, next) {
+        console.log(req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+        next();
+    }
+};
+
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    
-    logger: function(req, res, next) {
-        console.log(req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
-        next();
-    }
-};
+var middleware = require('./middleware');
 
 app.use(middleware.logger);
 app.use(middleware.requireAuthentication);
